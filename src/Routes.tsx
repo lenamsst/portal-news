36007eee0e2ac85e05d9cac9c346928918788678
Milena <lenamsst@gmail.com>
2022-05-02 15:59:33 -0300

    fix: ajuste

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,8 @@
-import {BrowserRouter, Routes as RoutesWrapper, Route} from "react-router-dom";
+import {
+    BrowserRouter, 
+    Routes as RoutesWrapper, 
+    Route} 
+    from "react-router-dom";
 import Home from "./pages/Home";
 
 function Routes() {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,10 @@ import Header from "../../components/Header";
 
 function Home() {
     return (
-    <div>
-        <Header />
-    </div>
-);
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default Home;
